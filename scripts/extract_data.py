5c0f6384c4aade525e8d5d3aa488d058da697100
jul-ian <julianaviles@gmail.com>
2023-03-11 11:42:08 -0800

    Continuing work on extracting single ticker dataframe from zip file

--- scripts/extract_data.py
+++ scripts/extract_data.py
@@ -5,6 +5,23 @@ The data/options.zip file takes up over 8GB and will take more space if all the 
 """
 
 import json
-fl = '/home/jul-ian/Github/ml-options/data/raw/AAPL.json'
-with open(fl, 'r') as f:
-    dat = json.load(f)
+from collections import defaultdict
+
+import pandas as pd
+
+json_file = '/home/jul-ian/Github/ml-options/data/raw/AAPL.json'
+with open(json_file, 'r') as f:
+    opt_dict = json.load(f)
+
+## Ameritrade
+if 'quote' in opt_dict.keys():
+    del opt_dict['quote']
+    df_dict = defaultdict(list)   
+    for date_key, data_list in opt_dict.items():
+        for data_dict in data_list:
+            del data_dict['OptionGreeks']
+            df_dict['date'] = df_dict['date'] + [date_key]
+            for key, item in data_dict.items():
+                df_dict[key] = df_dict[key] + [item]   
+        
+        
