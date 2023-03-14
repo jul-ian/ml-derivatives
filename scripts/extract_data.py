92f698b223a780e064213714066cd99b357ea60f
jul-ian <julianaviles@gmail.com>
2023-03-13 21:14:47 -0700

    Still working on extracting df from zip file.

--- scripts/extract_data.py
+++ scripts/extract_data.py
@@ -3,24 +3,42 @@
 The data/options.zip file takes up over 8GB and will take more space if all the files are unzipped. Since it is so much data, and I will be focusing on one stock at a time for now, this script will extract a dataframe from the files for a given stock ticker. 
 
 """
+import os
+from os.path import join
 import json
 import pandas as pd
 from zipfile import ZipFile
 from collections import defaultdict
 from tempfile import mkdtemp
 
-optzip_path = '/home/jul-ian/Github/ml-options/data/raw/options_2.zip'
-
-with ZipFile(optzip_path, 'r') as optzip:
-    nmlist = optzip.namelist()
-    
-    
-    
+ticksym = 'AAPL'
+tmpdir = mkdtemp()
 
 ameri_dfs = list()
 yahoo_dfs = list()
 
-json_file = '/home/jul-ian/Github/ml-options/data/raw/files/AAPL_AT.json'
+optzip_path = '/home/jul-ian/Github/ml-options/data/raw/options2.zip'
+with ZipFile(optzip_path, 'r') as optzip:
+    ziplist = [x for x in optzip.namelist() 
+               if x.startswith('options/') and x.endswith('zip')]  
+    for zipf in ziplist:
+        optzip.extract(zipf, path=tmpdir)
+        
+        with ZipFile(join(tmpdir, zipf), 'r') as datezip:
+            datezip.extract(f'{ticksym}.json', path=join(tmpdir, 'options'))
+        
+        source, dframe = json_to_dataframe(
+            join(tmpdir, 'options', f'{ticksym}.json')
+            )
+        if source == 'yahoo':
+            yahoo_dfs.append(dframe)
+        if source == 'ameritrade':
+            ameri_dfs.append(dframe)
+        os.remove(join(tmpdir, 'options', zipf))
+        os.remove(join(tmpdir, 'options', f'{ticksym}.json'))
+            
+
+#json_file = '/home/jul-ian/Github/ml-options/data/raw/files/AAPL_AT.json'
 
 def json_to_dataframe(json_path: str) -> pd.DataFrame:
     with open(json_path, 'r') as f:
@@ -35,13 +53,15 @@ def json_to_dataframe(json_path: str) -> pd.DataFrame:
             df_dict['date'] = df_dict['date'] + [date_key]
             for key, item in data_dict.items():
                 df_dict[key] = df_dict[key] + [item]
-    return pd.DataFrame(df_dict)
+    
+    if len(quote) == 68:
+        source = 'yahoo'
+    elif len(quote) == 58:
+        source = 'ameritrade'
+    else:
+        raise Exception('Neither Ameritrade nor Yahoo Finance')
+        
+    return source, pd.DataFrame(df_dict)
 
-# if Yahoo Finance
-if len(quote) == 68:
-    yahoo_dfs.append(pd.DataFrame(df_dict))
-elif len(quote) == 58:
-    ameri_dfs.append(pd.DataFrame(df_dict))
-else:
-    raise Exception('Neither Ameritrade nor Yahoo Finance')
+
 
