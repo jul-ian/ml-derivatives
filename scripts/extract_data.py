325c190f0d50498366bc5b15742d089dafb28b6b
jul-ian <julianaviles@gmail.com>
2023-03-19 23:06:05 -0700

    finished creating final df, will start working on df for risk free rate

--- scripts/extract_data.py
+++ scripts/extract_data.py
@@ -6,6 +6,7 @@ The data/options.zip file takes up over 8GB and will take more space if all the 
 import os
 from os.path import join
 import json
+import numpy as np
 import pandas as pd
 from zipfile import ZipFile
 from collections import defaultdict
@@ -39,8 +40,10 @@ with ZipFile(optzip_path, 'r') as optzip:
         os.remove(join(tmpdir, zipf))
         os.remove(join(tmpdir, 'options', f'{ticksym}.json'))
 
-df_yahoo = pd.concat(yahoo_dfs, ignore_index=True)
-df_ameri = pd.concat(ameri_dfs, ignore_index=True)
+df_yahoo = clean_opt_df(pd.concat(yahoo_dfs, ignore_index=True), 'yahoo')
+df_ameri = clean_opt_df(pd.concat(ameri_dfs, ignore_index=True), 'ameritrade')
+
+options_df = pd.concat([df_yahoo, df_ameri], ignore_index=True)
 
 def json_to_dataframe(json_path: str) -> tuple:
     with open(json_path, 'r') as f:
@@ -48,6 +51,8 @@ def json_to_dataframe(json_path: str) -> tuple:
 
     df_dict = defaultdict(list)   
     quote = opt_dict.pop('quote')
+    stock_bid = quote['bid']
+    stock_ask = quote['ask']
     for date_key, data_list in opt_dict.items():
         for data_dict in data_list:
             if 'OptionGreeks' in data_dict.keys():
@@ -56,33 +61,37 @@ def json_to_dataframe(json_path: str) -> tuple:
             else:
                 source = 'yahoo'
             df_dict['date_expired'] = df_dict['date_expired'] + [date_key]
+            df_dict['stock_bid'] = df_dict['stock_bid'] + [stock_bid]
+            df_dict['stock_ask'] = df_dict['stock_ask'] + [stock_ask]
             for key, item in data_dict.items():
                 df_dict[key] = df_dict[key] + [item]
         
     return source, pd.DataFrame(df_dict)
 
-def clean_ameri(df: pd.DataFrame) -> pd.DataFrame:
-    """
-    ['date',
-     'optionCategory',
-     'optionRootSymbol',
-     'timeStamp',
-     'adjustedFlag',
-     'displaySymbol',
-     'optionType',
-     'strikePrice',
-     'symbol',
-     'bid',
-     'ask',
-     'bidSize',
-     'askSize',
-     'inTheMoney',
-     'volume',
-     'openInterest',
-     'netChange',
-     'lastPrice',
-     'quoteDetail',
-     'osiKey']
-    """
 
+def clean_opt_df(df: pd.DataFrame, source: str) -> pd.DataFrame:
+    if source == 'ameritrade':
+        df = df.rename(columns={'optionType': 'option_type', 
+                   'strikePrice': 'strike_price',
+                   'bid': 'option_bid',
+                   'ask': 'option_ask'
+                   })
+        df['option_type'] = df['option_type'].str.lower()
+    elif source == 'yahoo':
+        df = df.rename(columns={'strike': 'strike_price',
+                   'bid': 'option_bid',
+                   'ask': 'option_ask'
+                   })
+        df['is_call'] = df['contractSymbol'].str.contains('^[A-Z\d]+C\d+$')
+        df['option_type'] = pd.Series(np.where(df.is_call, 'call', 'put'))
+        
+    
+    df['date_priced'] = pd.to_datetime(df['date_priced'])
+    df['date_expired']= pd.to_datetime(df['date_expired'])
+    
+    df['days_to_maturity'] = (df['date_expired'] - df['date_priced']).dt.days
 
+    return df[
+        ['date_priced', 'date_expired', 'days_to_maturity', 'stock_bid', 'stock_ask',
+         'option_bid', 'option_ask', 'strike_price', 'option_type']
+        ]
