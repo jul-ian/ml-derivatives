da242411907866bf608ea562d03fe7756c07cdc9
jul-ian <julianaviles@gmail.com>
2023-03-22 22:58:58 -0700

    Finished procesing treasury data, outputted final dataframe as pkl to data/processed

--- scripts/clean_tbill_data.py
+++ scripts/clean_tbill_data.py
@@ -13,17 +13,18 @@ for year in range(2018, 2021+1):
 
 rf_df = pd.concat(df_list, ignore_index=True)
 
-date_df = rf_df[['Date']]
-rates_df = rf_df[[c for c in risk_free_df.columns if 'DISCOUNT' in c]]
+dates = pd.to_datetime(rf_df['Date']).rename('date')
+rates_df = rf_df[[c for c in rf_df.columns if 'DISCOUNT' in c]]
 rates_df = rates_df.applymap(lambda d: (1 - d/100)**(-1) - 1)
 rename_map = {
- '4 WEEKS BANK DISCOUNT': 'rf_4wk',
- '8 WEEKS BANK DISCOUNT': 'rf_8wk',
- '13 WEEKS BANK DISCOUNT': 'rf_13wk',
- '26 WEEKS BANK DISCOUNT': 'rf_26wk',
- '52 WEEKS BANK DISCOUNT': 'rf_52wk'
+ '4 WEEKS BANK DISCOUNT': 'rate_4wk',
+ '8 WEEKS BANK DISCOUNT': 'rate_8wk',
+ '13 WEEKS BANK DISCOUNT': 'rate_13wk',
+ '26 WEEKS BANK DISCOUNT': 'rate_26wk',
+ '52 WEEKS BANK DISCOUNT': 'rate_52wk'
  }
-rates_df = rates_df.rename(columns=rename_map)
-
-risk_free_df = pd.merge(date_df, rates_df, left_index=True, right_index=True)
-
+risk_free_df = (
+    pd.merge(dates, rates_df, left_index=True, right_index=True)
+    .rename(columns=rename_map)
+    )
+risk_free_df.to_pickle(os.path.join(datadir, 'processed', 'risk_free_df.pkl'))
